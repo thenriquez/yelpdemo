46053122ef2aee26445fff08ac49606947e80814
Terrence <thenriquez25@outlook.com>
2016-11-18 01:36:51 -0500

    pushing up files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yelpdemo::Application.config.secret_token = 'eac9582eb2afd73be4e9f7c59c25644aaf152919d400011e4dd62dbcff43cb4cf9aa990bcf55c91d80c3d2538265af8bb62e69e8c84d019192e4702415f637a3'
